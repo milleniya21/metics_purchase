c7ca1f818ccf6f4b2067fa775f0a6a5325e603f8
milleniya21 <milleniyasajy21@gmail.com>
2023-02-20 12:15:02 +0530

    table,cards_completed

---
+++ purchase/src/App.js
@@ -0,0 +1,18 @@
+
+import Header from './header/header';
+ import Basic from './table/Basic';
+import Title from './title/title';
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <Header/>
+       <Title/>  
+       <Basic/> 
+    </div>
+  );
+}
+
+export default App;
